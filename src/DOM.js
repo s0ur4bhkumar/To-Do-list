2d94e5558f1ca93650d3a3bade1966726c3aa58b
s0ur4bhkumar <soul65917@gmail.com>
2026-02-06 20:08:08 +0530

    fix project displag bug which was not displaying the project properly

--- src/DOM.js
+++ src/DOM.js
@@ -91,6 +91,7 @@ function newProjectOption(title, value) {
 }
 
 function template(objlist, title) {
+  // console.log(objlist);
   const Card = document.createElement("div");
   const btnContainer = document.createElement("div");
   const doneBtn = document.createElement("button");
@@ -115,21 +116,89 @@ function template(objlist, title) {
   });
 }
 
+function Home() {
+  const container = document.querySelector(".content");
+  container.innerHTML = "";
+  const lst = JSON.parse(localStorage.getItem("project"));
+  // console.log(lst);
+  if (lst.length !== 0) {
+    lst.forEach((task) => {
+      if (task.Title === "Home") {
+        Object.values(task).forEach((val) => {
+          if (val !== "Home") {
+            const card = document.createElement("div");
+            const btnContainer = document.createElement("div");
+            const doneBtn = document.createElement("button");
+            const delBtn = document.createElement("button");
+            doneBtn.textContent = "Done";
+            delBtn.textContent = "Delete";
+            doneBtn.classList.add("doneBtn");
+            delBtn.classList.add("delBtn");
+            btnContainer.append(doneBtn, delBtn);
+            card.classList.add("homeCard");
+            for (const [key, value] of Object.entries(val)) {
+              if (key !== "Id" && key !== "createdBy") {
+                const p = document.createElement("p");
+                p.textContent = `${key}: ${value}`;
+                card.append(p, btnContainer);
+              }
+            }
+            container.append(card);
+          }
+        });
+      }
+    });
+  }
+}
+
+function projectDisplay(objlist, title) {
+  const container = document.querySelector('.content');
+  objlist.forEach(obj => {
+    if (obj.Title !== 'Home') {
+      // const card = document.createElement("div");
+      // const btnContainer = document.createElement("div");
+      // const doneBtn = document.createElement("button");
+      // const delBtn = document.createElement("button");
+      // doneBtn.textContent = "Done";
+      // delBtn.textContent = "Delete";
+      // doneBtn.classList.add("doneBtn");
+      // delBtn.classList.add("delBtn");
+      // btnContainer.append(doneBtn, delBtn);
+      // card.classList.add("homeCard");
+      return obj
+    }
+  })
+}
+
+function addHome() {
+  const projectContainer = document.querySelector(".Project-list");
+  const projectHome = document.createElement("div");
+  projectHome.textContent = "Home";
+  projectHome.classList.add("Home");
+  projectContainer.append(projectHome);
+  projectHome.addEventListener("click", () => {
+    Home();
+  });
+}
+
 function addToContent(e) {
   container.innerHTML = "";
   // console.log('add: ', e.target.className)
-  let todoList;
-  if (e.target.className === "project") {
-    console.log("yup");
-    todoList = JSON.parse(localStorage.getItem("project"));
+  let Title;
+  let List;
+  if (e.target.classList.contains("project")) {
+    List = JSON.parse(localStorage.getItem("project"));
+    Title = e.target.querySelector("p").textContent;
+    console.log('for project display: ',projectDisplay(List,Title))
+    e.target.classList.add(`${Title}`);
   } else {
-    todoList = JSON.parse(localStorage.getItem("todo"));
+    List = JSON.parse(localStorage.getItem("todo"));
+    Title = e.target.querySelector("p").textContent;
+    e.target.classList.add(`${Title}`);
   }
-  console.log(todoList);
-  const Title = e.target.querySelector("p").textContent;
-  template(todoList, Title);
+  console.log(List);
+  template(List, Title);
   // container.textContent = `hello: brother`;
-  e.target.classList.add(`${Title}`);
 }
 
 // *********************************************DOM manipulation*********************************************
@@ -144,14 +213,14 @@ const projects = projectList;
 const taskList = todoList;
 
 tasks.addEventListener("click", (e) => {
-  if (e.target.className === "task") {
-    console.log(e.target);
+  if (e.target.classList.contains("task")) {
+    // console.log(e.target);
     addToContent(e);
   }
 });
 
 projectContainer.addEventListener("click", (e) => {
-  if (e.target.className === "project") {
+  if (e.target.classList.contains("project")) {
     addToContent(e);
   }
 });
@@ -167,3 +236,7 @@ myDialog.forEach((dialog) =>
     addToContainer(dialog);
   }),
 );
+
+addHome();
+Home();
+// addHome();
